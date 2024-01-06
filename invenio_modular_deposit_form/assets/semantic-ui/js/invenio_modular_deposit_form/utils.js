7342439647b8b6d34c35039a89de770e4f0dc92f
Ian Scott <scottianw@gmail.com>
2024-01-06 17:55:23 -0500

    Utility functions for object deep comparison

--- invenio_modular_deposit_form/assets/semantic-ui/js/invenio_modular_deposit_form/utils.js
+++ invenio_modular_deposit_form/assets/semantic-ui/js/invenio_modular_deposit_form/utils.js
@@ -81,7 +81,73 @@ function getTouchedParent(touched, fieldPath) {
   return touchedAncestor;
 }
 
+// Compares two objects deeply, ignoring the keys passed in the ignoreKeys array
+// Returns true if the objects are deeply equal, false otherwise
+// If ignoreKeys is not passed, it compares the objects deeply without ignoring any keys
+//
+// param obj1: first object to compare
+// param obj2: second object to compare
+// param ignoreKeys: array of keys to ignore when comparing the objects
+// returns: true if the objects are deeply equal, false otherwise
+function areDeeplyEqual(obj1, obj2, ignoreKeys) {
+  if (ignoreKeys) {
+    obj1 = excludeObjectKeys(obj1, ignoreKeys);
+    obj2 = excludeObjectKeys(obj2, ignoreKeys);
+  }
+  if (obj1 === obj2) return true;
+
+  if (Array.isArray(obj1) && Array.isArray(obj2)) {
+    if (obj1.length !== obj2.length) return false;
+
+    return obj1.every((elem, index) => {
+      return areDeeplyEqual(elem, obj2[index]);
+    });
+  }
+
+  if (
+    typeof obj1 === "object" &&
+    typeof obj2 === "object" &&
+    obj1 !== null &&
+    obj2 !== null
+  ) {
+    if (Array.isArray(obj1) || Array.isArray(obj2)) return false;
+
+    const keys1 = Object.keys(obj1);
+    const keys2 = Object.keys(obj2);
+
+    if (
+      keys1.length !== keys2.length ||
+      !keys1.every((key) => keys2.includes(key))
+    )
+      return false;
+
+    for (let key in obj1) {
+      console.log(obj1[key], obj2[key]);
+      let isEqual = areDeeplyEqual(obj1[key], obj2[key]);
+      if (!isEqual) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+
+  return false;
+}
+
+function excludeObjectKeys(obj, keys) {
+  const newObj = {};
+  for (const key in obj) {
+    if (!keys.includes(key)) {
+      newObj[key] = obj[key];
+    }
+  }
+  return newObj;
+}
+
 export {
+  areDeeplyEqual,
+  excludeObjectKeys,
   getTouchedParent,
   scrollTop,
   moveToArrayStart,
