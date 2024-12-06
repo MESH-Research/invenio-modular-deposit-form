68a24a1d2cfd4a704c6c675233caa00e5a3aedba
Ian Scott <scottianw@gmail.com>
2024-12-06 11:45:27 -0500

    fix(build): Updating versions to use new string format

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Invenio Modular Deposit Form
 
-Version 0.1.0-dev1
+Version 0.2.0-dev0
 
 **ALPHA**
 
--- invenio_modular_deposit_form/__init__.py
+++ invenio_modular_deposit_form/__init__.py
@@ -10,6 +10,6 @@
 
 from .ext import InvenioModularDepositForm
 
-__version__ = "0.1.0"
+__version__ = "0.2.0-dev0"
 
 __all__ = ("__version__", "InvenioModularDepositForm")
