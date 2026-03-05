67ddd894d2da31183a5ba46e14b934b3984d20f2
Ian W. Scott <scottianw@gmail.com>
2026-03-04 22:43:31 -0500

    chore: Update dates in docstrings.

--- run-tests.sh
+++ run-tests.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # -*- coding: utf-8 -*-
 #
-# Copyright (C) 2023 Mesh Research.
+# Copyright (C) 2023-2026 Mesh Research.
 #
 # Invenio Modular Deposit Form is free software; you can redistribute it
 # and/or modify it under the terms of the MIT License; see LICENSE file for
