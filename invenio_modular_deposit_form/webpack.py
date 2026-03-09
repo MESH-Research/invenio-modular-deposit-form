8467068746ded49e2d2493036e80bc3880a65c18
Ian W. Scott <scottianw@gmail.com>
2026-03-09 19:38:13 -0400

    fix(config): Remove bad webpack alias.

--- invenio_modular_deposit_form/webpack.py
+++ invenio_modular_deposit_form/webpack.py
@@ -6,10 +6,10 @@ from .webpack_extras import get_components_registry_path, get_validator_path
 _STUBS_PATH = "js/invenio_modular_deposit_form/stubs"
 
 _aliases = {
-    "@js/invenio_modular_deposit_form": "js/invenio_modular_deposit_form",
     "@translations/invenio_modular_deposit_form": "translations/invenio_modular_deposit_form",
     "@js/invenio_modular_deposit_form_validator": get_validator_path() or _STUBS_PATH,
-    "@js/invenio_modular_deposit_form_components": get_components_registry_path() or _STUBS_PATH,
+    "@js/invenio_modular_deposit_form_components": get_components_registry_path()
+    or _STUBS_PATH,
 }
 
 theme = WebpackThemeBundle(
