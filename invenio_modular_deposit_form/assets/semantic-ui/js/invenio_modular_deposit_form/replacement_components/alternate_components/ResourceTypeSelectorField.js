51530e4e1be6f5457123cec806e2c069c9945f46
Ian W. Scott <scottianw@gmail.com>
2026-04-08 14:55:41 -0400

    refactor(resource-type): Add improved aria props and keyboard nav to widget.

--- invenio_modular_deposit_form/assets/semantic-ui/js/invenio_modular_deposit_form/replacement_components/alternate_components/ResourceTypeSelectorField.js
+++ invenio_modular_deposit_form/assets/semantic-ui/js/invenio_modular_deposit_form/replacement_components/alternate_components/ResourceTypeSelectorField.js
@@ -7,14 +7,24 @@
 // you can redistribute them and/or modify them
 // under the terms of the MIT License; see LICENSE file for more details.
 
-import React, { useEffect, useMemo, useState } from "react";
+import React, { useCallback, useEffect, useMemo, useRef, useState } from "react";
 import PropTypes from "prop-types";
 import { Form } from "semantic-ui-react";
-import { FieldLabel, SelectField } from "react-invenio-forms";
 import { i18next } from "@translations/invenio_modular_deposit_form/i18next";
+import { SelectField } from "../SelectField";
+import { FieldLabel } from "../FieldLabel";
 import { Field, getIn, useFormikContext } from "formik";
 import { Icon, Label } from "semantic-ui-react";
 import { useStore } from "react-redux";
+
+const RADIO_GROUP_NAV_KEYS = new Set([
+  "ArrowDown",
+  "ArrowRight",
+  "ArrowUp",
+  "ArrowLeft",
+  "Home",
+  "End",
+]);
 
 const ResourceTypeSelectorField = ({
   classnames = undefined,
@@ -26,6 +36,7 @@ const ResourceTypeSelectorField = ({
   options: optionsProp,
   labelclassname = "field-label-class",
   required,
+  showLabel = true,
   shortcutResourceTypeIds: shortcutResourceTypeIdsProp,
   ...uiProps
 }) => {
@@ -37,6 +48,7 @@ const ResourceTypeSelectorField = ({
   const { values, setFieldValue } = useFormikContext();
   const currentTypeId = getIn(values, fieldPath);
   const [otherToggleActive, setOtherToggleActive] = useState(false);
+  const radioRefs = useRef([]);
 
   const optionById = useMemo(() => {
     const map = {};
@@ -95,6 +107,114 @@ const ResourceTypeSelectorField = ({
   };
   const frontEndOptions = createOptions(options);
 
+  const safeFieldId = fieldPath.replaceAll(".", "-").replaceAll(":", "-");
+  const labelElementId = `${fieldPath}.label`;
+  const errorMessageId = `${fieldPath}.error`;
+  const descriptionElementId =
+    description && description !== " " ? `${fieldPath}.description` : null;
+  const helpTextElementId = helpText && helpText !== " " ? `${fieldPath}.helptext` : null;
+  const otherListId = `${safeFieldId}-other-select`;
+
+  const radioCount = shortcutButtons.length + 1;
+
+  const focusOtherSelectInput = useCallback(() => {
+    const wrapperClass = `${fieldPath.replaceAll(".", "-").replaceAll(":", "-")}-field`;
+    setTimeout(() => {
+      document
+        .querySelector(`.invenio-field-wrapper.${wrapperClass} .invenio-select-field input`)
+        ?.focus();
+    }, 200);
+  }, [fieldPath]);
+
+  const moveRadioSelection = useCallback(
+    (nextIndex) => {
+      if (radioCount < 1) {
+        return;
+      }
+      const i = ((nextIndex % radioCount) + radioCount) % radioCount;
+      if (i < shortcutButtons.length) {
+        setFieldValue(fieldPath, shortcutButtons[i].id);
+        setOtherToggleActive(false);
+        queueMicrotask(() => radioRefs.current[i]?.focus());
+      } else {
+        if (frontEndOptions.length < 1) {
+          return;
+        }
+        setFieldValue(fieldPath, frontEndOptions[0].value);
+        setOtherToggleActive(true);
+        queueMicrotask(() => {
+          radioRefs.current[shortcutButtons.length]?.focus();
+          focusOtherSelectInput();
+        });
+      }
+    },
+    [fieldPath, focusOtherSelectInput, frontEndOptions, radioCount, setFieldValue, shortcutButtons]
+  );
+
+  const handleRadioGroupKeyDown = useCallback(
+    (event) => {
+      if (!RADIO_GROUP_NAV_KEYS.has(event.key)) {
+        return;
+      }
+      if (event.target.getAttribute("role") !== "radio") {
+        return;
+      }
+      const radios = radioRefs.current.filter(Boolean);
+      if (radios.length === 0) {
+        return;
+      }
+      const idx = radios.indexOf(event.target);
+      if (idx < 0) {
+        return;
+      }
+      event.preventDefault();
+
+      if (event.key === "Home") {
+        moveRadioSelection(0);
+        return;
+      }
+      if (event.key === "End") {
+        moveRadioSelection(radioCount - 1);
+        return;
+      }
+      const forward = event.key === "ArrowDown" || event.key === "ArrowRight";
+      const backward = event.key === "ArrowUp" || event.key === "ArrowLeft";
+      if (forward) {
+        moveRadioSelection(idx + 1);
+      } else if (backward) {
+        moveRadioSelection(idx - 1);
+      }
+    },
+    [moveRadioSelection, radioCount]
+  );
+
+  const getRadioTabIndex = (index) => {
+    const otherIndex = shortcutButtons.length;
+    const isOther = index === otherIndex;
+
+    if (shortcutButtons.length === 0) {
+      return 0;
+    }
+    if (otherToggleActive) {
+      return isOther ? 0 : -1;
+    }
+    if (!isOther) {
+      const id = shortcutButtons[index].id;
+      if (currentTypeId === id) {
+        return 0;
+      }
+      if (!currentTypeId && index === 0) {
+        return 0;
+      }
+      return -1;
+    }
+    return -1;
+  };
+
+  const assignRadioRef = (index) => (el) => {
+    radioRefs.current[index] = el;
+  };
+
   const handleItemClick = (event) => {
     const btn = event.target.closest("button");
     if (!btn?.name) {
@@ -110,74 +230,126 @@ const ResourceTypeSelectorField = ({
     }
     setFieldValue(fieldPath, frontEndOptions[0].value);
     setOtherToggleActive(true);
-
-    const wrapperClass = `${fieldPath.replaceAll(".", "-").replaceAll(":", "-")}-field`;
-    setTimeout(() => {
-      document
-        .querySelector(`.invenio-field-wrapper.${wrapperClass} .invenio-select-field input`)
-        ?.focus();
-    }, 200);
+    focusOtherSelectInput();
   };
+
+  const firstShortcutHtmlFor =
+    shortcutButtons.length > 0
+      ? `${safeFieldId}-opt-${shortcutButtons[0].id}`
+      : `${safeFieldId}-other-toggle`;
+
+  const assistiveGroupName =
+    typeof label === "string" && label.trim() !== "" ? label : i18next.t("Resource type");
 
   return (
     <Field id={fieldPath} name={fieldPath}>
-      {({ meta }) => (
-        <Form.Field required={!!required} error={!!meta.error && !!meta.touched}>
-          {label && <FieldLabel htmlFor={fieldPath} icon={icon} label={label} />}
-          {description && description !== " " && (
-            <label className="helptext label top">{i18next.t(description)}</label>
-          )}
-          <div className="ui compact fluid icon labeled six item menu">
-            {shortcutButtons.map((buttonType) => (
-              <button
-                key={buttonType.id}
-                id={buttonType.id}
-                name={buttonType.id}
-                onClick={handleItemClick}
-                className={`ui button item ${currentTypeId === buttonType.id ? "active" : ""}`}
-                formNoValidate
-                type="button"
-              >
-                <Icon name={buttonType.icon} />
-                {buttonType.label}
-              </button>
-            ))}
-            <button
-              id={"otherToggle"}
-              name={"otherToggle"}
-              onClick={handleOtherToggleClick}
-              className={`ui button item pt-25 pb-25 ${otherToggleActive === true ? "active" : ""}`}
-              formNoValidate
-              type="button"
+      {({ meta }) => {
+        const showError = !!meta.error && !!meta.touched;
+        const describedBy = [
+          showError ? errorMessageId : null,
+          helpTextElementId,
+          descriptionElementId,
+        ]
+          .filter(Boolean)
+          .join(" ");
+
+        return (
+          <Form.Field required={!!required} error={showError}>
+            {showLabel && label ? (
+              <FieldLabel
+                className={labelclassname}
+                htmlFor={firstShortcutHtmlFor}
+                icon={icon}
+                id={labelElementId}
+                label={label}
+              />
+            ) : null}
+            {description && description !== " " && (
+              <div className="description label top" id={descriptionElementId} htmlFor={undefined}>
+                {i18next.t(description)}
+              </div>
+            )}
+            <div
+              className="ui compact fluid icon labeled six item menu"
+              role="radiogroup"
+              aria-invalid={showError || undefined}
+              aria-labelledby={showLabel && label ? labelElementId : undefined}
+              aria-label={showLabel && label ? undefined : assistiveGroupName}
+              aria-describedby={describedBy || undefined}
+              onKeyDown={handleRadioGroupKeyDown}
             >
-              <Icon name="asterisk" />
-              Other...
-            </button>
-          </div>
-          {!!otherToggleActive && (
-            <SelectField
-              fieldPath={fieldPath}
-              label={""}
-              optimized
-              options={frontEndOptions}
-              selectOnBlur={true}
-              selectOnNavigation={true}
-              search={true}
-              placeholder={"choose another resource type..."}
-              upward={false}
-              {...uiProps}
-            />
-          )}
-          {meta.error && meta.touched && (
-            <Label pointing className="prompt error">
-              {meta.error}
-            </Label>
-          )}
-          {helpText && helpText !== " " && (
-            <label className="helptext">{i18next.t(helpText)}</label>
-          )}
-        </Form.Field>
-      )}
+              {shortcutButtons.map((buttonType, index) => {
+                const checked = !otherToggleActive && currentTypeId === buttonType.id;
+                return (
+                  <button
+                    key={buttonType.id}
+                    ref={assignRadioRef(index)}
+                    id={`${safeFieldId}-opt-${buttonType.id}`}
+                    name={buttonType.id}
+                    type="button"
+                    role="radio"
+                    tabIndex={getRadioTabIndex(index)}
+                    aria-checked={checked}
+                    onClick={handleItemClick}
+                    className={`ui button item ${checked ? "active" : ""}`}
+                    formNoValidate
+                  >
+                    <Icon aria-hidden="true" name={buttonType.icon} />
+                    {buttonType.label}
+                  </button>
+                );
+              })}
+              <button
+                ref={assignRadioRef(shortcutButtons.length)}
+                id={`${safeFieldId}-other-toggle`}
+                name="otherToggle"
+                type="button"
+                role="radio"
+                tabIndex={getRadioTabIndex(shortcutButtons.length)}
+                aria-checked={otherToggleActive}
+                aria-haspopup="listbox"
+                aria-expanded={otherToggleActive}
+                aria-controls={otherListId}
+                onClick={handleOtherToggleClick}
+                className={`ui button item pt-25 pb-25 ${otherToggleActive ? "active" : ""}`}
+                formNoValidate
+              >
+                <Icon aria-hidden="true" name="asterisk" />
+                Other...
+              </button>
+            </div>
+            {!!otherToggleActive && (
+              <SelectField
+                {...uiProps}
+                fieldPath={fieldPath}
+                label=""
+                optimized
+                options={frontEndOptions}
+                selectOnBlur={true}
+                selectOnNavigation={true}
+                search={true}
+                placeholder={i18next.t("choose another resource type...")}
+                upward={false}
+                id={otherListId}
+                aria-labelledby={showLabel && label ? labelElementId : undefined}
+                aria-label={showLabel && label ? undefined : assistiveGroupName}
+                aria-describedby={describedBy || undefined}
+                aria-invalid={showError || undefined}
+              />
+            )}
+            {showError && (
+              <Label id={errorMessageId} pointing className="prompt error" role="alert">
+                {meta.error}
+              </Label>
+            )}
+            {helpText && helpText !== " " && (
+              <div className="helptext" id={helpTextElementId} htmlFor={undefined}>
+                {i18next.t(helpText)}
+              </div>
+            )}
+          </Form.Field>
+        );
+      }}
     </Field>
   );
 };
@@ -198,6 +370,7 @@ ResourceTypeSelectorField.propTypes = {
     }).isRequired
   ),
   required: PropTypes.bool,
+  showLabel: PropTypes.bool,
   shortcutResourceTypeIds: PropTypes.arrayOf(PropTypes.string),
 };
 
