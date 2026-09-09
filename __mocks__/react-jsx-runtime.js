a78e87ba482cef7162dab979905c4489fa4090ec
Ian W. Scott <scottianw@gmail.com>
2026-09-09 17:34:33 -0400

    testing: fix jsx-runtime shim for jest suite

--- __mocks__/react-jsx-runtime.js
+++ __mocks__/react-jsx-runtime.js
@@ -1,12 +1,21 @@
 /**
  * Jest shim: @babel/preset-react "automatic" imports react/jsx-runtime, which exists
  * only from React 17+. This package tests against React 16.13.
+ *
+ * Automatic runtime calls `jsx(type, props, key)` / `jsxs(type, props, key)` where the
+ * third argument is the React **key**, not a child.
  */
 const React = require("react");
 
+function jsx(type, props, key) {
+  const nextProps =
+    key !== undefined && key !== null ? { ...props, key } : props || {};
+  return React.createElement(type, nextProps);
+}
+
 module.exports = {
   Fragment: React.Fragment,
-  jsx: React.createElement,
-  jsxs: React.createElement,
-  jsxDEV: React.createElement,
+  jsx,
+  jsxs: jsx,
+  jsxDEV: jsx,
 };
